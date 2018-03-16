1de80f4560dd175c63d5a7b9310c1aa196d65083
Carsten Gräf <evandor@gmail.com>
2018-03-16 17:29:36 +0100

    content

--- vars/stageDeploy.groovy
+++ vars/stageDeploy.groovy
@@ -1,3 +1,3 @@
 def call(app, env, version) {
-    sh "nohup /home/carsten/install/docker/skysail/run_docker.sh "+app+" "+env+" "+version
+    sh "nohup /home/carsten/install/docker/skysail/run_docker.sh " + app + " " + env + " " + version
 }
