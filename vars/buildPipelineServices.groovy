d3a6846888d633103d8e9fafff2bac9119a309f3
Carsten Gräf <evandor@gmail.com>
2018-04-10 09:11:58 +0200

    various

---
+++ vars/buildPipelineServices.groovy
@@ -0,0 +1,73 @@
+/**
+ * Pipeline Definition File
+ * Information about the syntax can be found here: https://jenkins.io/doc/book/pipeline/syntax/
+ *
+ * To run this File as a Pipeline job atleast the following plugins are required:
+ * https://wiki.jenkins-ci.org/display/JENKINS/Pipeline+Model+Definition+Plugin
+ * https://wiki.jenkins-ci.org/display/JENKINS/Pipeline+Utility+Steps+Plugin
+ * https://wiki.jenkins-ci.org/display/JENKINS/Lockable+Resources+Plugin
+ */
+
+def call(project) {
+
+    env.PROJECT_NAME = project
+
+    pipeline {
+        agent any
+
+        options {
+            buildDiscarder(logRotator(numToKeepStr: '20'))
+        }
+
+        stages {
+
+            stage('Prepare') {
+                steps {
+                    stagePrepareServices()
+                }
+
+            }
+
+            stage('Build') {
+                steps {
+                    sh "./gradlew -Pversion=${env.BUILD_VERSION} -DbuildVersion=jenkins-${env.BUILD_VERSION} --stacktrace --continue clean build"
+                    withCredentials([usernamePassword(credentialsId: 'd04cfe1a-4efc-4a0a-b65b-4775a1a15a14',
+                            usernameVariable: 'ACCESS_TOKEN_USERNAME',
+                            passwordVariable: 'ACCESS_TOKEN_PASSWORD',)]) {
+                        sh "git remote set-url origin https://$ACCESS_TOKEN_USERNAME:$ACCESS_TOKEN_PASSWORD@github.com/evandor/services"
+                        sh "git tag -m '' ${env.BUILD_VERSION}"
+                        sh "git pull --tags"
+                        sh "git push --tags"
+                    }
+                }
+                post {
+                    always {
+                        junit "**/test-reports/test/TEST-*.xml"
+                    }
+                }
+            }
+
+
+            stage ('Document') {
+                steps {
+                    //sh "./gradlew asciidoctor"
+                    sh "./gradlew scaladoc"
+                }
+            }
+
+        }
+        post {
+            failure {
+                emailext body: '$DEFAULT_CONTENT',
+                        recipientProviders: [
+                                [$class: 'CulpritsRecipientProvider'],
+                                [$class: 'DevelopersRecipientProvider'],
+                                [$class: 'RequesterRecipientProvider']
+                        ],
+                        replyTo: '$DEFAULT_REPLYTO',
+                        subject: '$DEFAULT_SUBJECT',
+                        to: '$DEFAULT_RECIPIENTS'
+            }
+        }
+    }
+}
