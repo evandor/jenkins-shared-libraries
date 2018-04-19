a72f17d0c1470408058454f8843f6e0ecbacb6c3
Carsten Gräf <evandor@gmail.com>
2018-04-19 08:32:37 +0200

    docker

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -75,7 +75,7 @@ def call(project) {
                         //sh "git pull --rebase"
 
                         withEnv(['JENKINS_NODE_COOKIE =dontkill']) {
-                            sh "/home/carsten/install/docker/services/run_docker.sh skysail-service-monitor test latest &"
+                            sh "/home/carsten/install/docker/services/run_docker.sh skysail-service-monitor test 0.1.11 &"
                         }
                     }
                 }
