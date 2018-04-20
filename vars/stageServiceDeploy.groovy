32ea2f2d3eead03243e82a39f0e39f2dd1207818
Carsten Gräf <evandor@gmail.com>
2018-04-20 08:14:47 +0200

    docker on new site

--- vars/stageServiceDeploy.groovy
+++ vars/stageServiceDeploy.groovy
@@ -1,3 +1,3 @@
 def call(app, env, version) {
-    sh "nohup /home/carsten/install/docker/service/run_docker.sh " + app + " " + env + " " + version
+    sh "nohup /home/carsten/install/docker/services/run_docker.sh " + app + " " + env + " " + version
 }
