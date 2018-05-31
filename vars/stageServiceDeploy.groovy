711f8d5f471356d483bd2274c25abd4647853c32
Carsten Gräf <evandor@gmail.com>
2018-05-31 18:27:43 +0200

    no host no more

--- vars/stageServiceDeploy.groovy
+++ vars/stageServiceDeploy.groovy
@@ -1,3 +1,3 @@
 def call(app, env, version) {
-    sh "nohup /home/carsten/install/docker/services/run_docker_with_hostnet.sh " + app + " " + env + " " + version
+    sh "nohup /home/carsten/install/docker/services/run_docker.sh " + app + " " + env + " " + version
 }
