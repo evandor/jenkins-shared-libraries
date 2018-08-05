67595676e219ca1255124704429fd963cbe20d1c
Carsten Graef <evandor@gmail.com>
2018-08-05 09:19:43 +0200

    wip

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -101,7 +101,7 @@ def call(project, modulePath, runGatling) {
             stage('Gatling') {
                 when {
                     expression {
-                        return env.RUN_GATLING;
+                        return env.RUN_GATLING == true; //???
                     }
                 }
                 steps {
