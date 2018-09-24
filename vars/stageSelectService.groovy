bf5be39cf9531205e19186e7f0e7ad45e6199426
Carsten Graef <evandor@gmail.com>
2018-09-24 07:45:13 +0200

    adding search page

--- vars/stageSelectService.groovy
+++ vars/stageSelectService.groovy
@@ -3,7 +3,7 @@ def call() {
     env.APPLICATION_NAME =
             input(id: 'env.APPLICATION_NAME', message: 'Welche Anwendung soll deployed werden?', parameters: [
                     [$class     : 'ChoiceParameterDefinition',
-                     choices    : ["skysail-service-monitor","rse"],
+                     choices    : "skysail-service-monitor\nrse",
                      description: 'Welche Instanz soll deployed werden?', name: 'application']
             ])
 }
