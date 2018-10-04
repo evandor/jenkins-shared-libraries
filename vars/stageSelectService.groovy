026d7a345e88fa16f369eeccd0dfb482d380c82e
Carsten Graef <evandor@gmail.com>
2018-10-04 09:08:04 +0200

    preparing notes-int

--- vars/stageSelectService.groovy
+++ vars/stageSelectService.groovy
@@ -3,7 +3,9 @@ def call() {
     env.APPLICATION_NAME =
             input(id: 'env.APPLICATION_NAME', message: 'Welche Anwendung soll deployed werden?', parameters: [
                     [$class     : 'ChoiceParameterDefinition',
-                     choices    : "skysail-service-monitor\nrse\nrse-web-client",
+                     choices    : "skysail-service-monitor\n" +
+                             "rse\nrse-web-client\n" +
+                             "notes\nnotes-web-client",
                      description: 'Welche Instanz soll deployed werden?', name: 'application']
             ])
 }
