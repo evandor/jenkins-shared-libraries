16698a2161d3068bd990dadbc32502f75a233ad0
Carsten Gräf <carsten.graef_extern-cegeka@kvb.de>
2018-10-30 16:09:22 +0100

    adding e2e test stage (as new standard)

--- vars/buildPipelineAngular2.groovy
+++ vars/buildPipelineAngular2.groovy
@@ -56,6 +56,15 @@ def call(project, modulePath) {
                 }
             }
 
+            stage('E2E Tests') {
+                steps {
+                    script {
+                        // -Dgeb.build.baseUrl="http://localhost:4200/" chromeTest
+                        sh "cd web-client-e2e-test && ./gradlew -Dgeb.build.baseUrl='http://notes.test.skysail.io/' test"
+                    }
+                }
+            }
+
         }
         post {
             failure {
