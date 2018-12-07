5ff7de1b53d93b2038ebeac7c4f2f4165b317457
Carsten Graef <evandor@gmail.com>
2018-12-07 07:04:30 +0100

    wip

--- vars/buildPipeline6.groovy
+++ vars/buildPipeline6.groovy
@@ -70,15 +70,8 @@ def call(project, modulePath, theStage) {
                 steps{
                     // see https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
                     sshagent(credentials : ['sailor1']) {
-                        // ${project} ${theStage} ${env.BUILD_VERSION}
                         sh 'ssh -o StrictHostKeyChecking=no carsten@185.183.96.103 uptime ${project}'
-                        sh "ssh -o StrictHostKeyChecking=no carsten@185.183.96.103 docker run --name  ${project}"
-                        sh """
-                            ssh -o BatchMode=yes -o StrictHostKeyChecking=no carsten@185.183.96.103 docker run --name ${project}-${stage}
-                           """
-                        //sh "ssh -o BatchMode=yes -o StrictHostKeyChecking=no carsten@185.183.96.103 docker run --name ${project}-${stage} --restart on-failure:1 --dns 85.25.128.10 -d -t -e spring.profiles.active=${stage} -e config.resource=/${stage}.conf -p 6003:8183 evandor/${project}:${version}"
-                        //sh 'ssh -v user@hostname.com'
-                        //sh 'scp ./source/filename user@hostname.com:/remotehost/target'
+                        sh "ssh -o StrictHostKeyChecking=no carsten@185.183.96.103 dockerRun  ${project} ${stage} ${env.BUILD_VERSION}"
                     }
                 }
             }
