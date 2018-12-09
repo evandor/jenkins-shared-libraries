8ac48c18fb225617e688c66f7615e0a5307ece17
Carsten Graef <evandor@gmail.com>
2018-12-09 19:32:31 +0100

    wip

---
+++ vars/httpdDeploy.groovy
@@ -0,0 +1,32 @@
+def call(user, target) {
+
+    pipeline {
+        agent any
+
+        options {
+            buildDiscarder(logRotator(numToKeepStr: '20'))
+        }
+
+        stages {
+
+            stage('Prepare') {
+                steps {
+                    //stagePrepareServices()
+                    checkout scm
+                }
+
+            }
+
+            stage ("Copy dockerRun to $target") {
+                steps{
+                    sshagent(credentials : ['aws-ec2-34-246-151-3']) {
+                        sh "ssh -o StrictHostKeyChecking=no $user@$target uptime ${project}"
+                        sh "ssh -o StrictHostKeyChecking=no $user@$target whoami"
+                        //sh 'scp -o StrictHostKeyChecking=no docker/dockerRun carsten@185.183.96.103:/home/carsten/bin/dockerRun'
+                    }
+                }
+            }
+
+        }
+    }
+}
