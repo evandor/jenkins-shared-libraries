400774be7be46c866e2d9e374fe11c946095d076
Carsten Graef <evandor@gmail.com>
2018-12-09 20:15:31 +0100

    wip

--- vars/httpdDeploy.groovy
+++ vars/httpdDeploy.groovy
@@ -22,7 +22,7 @@ def call(user, target) {
                     sshagent(credentials : ['sailor1']) {
                         sh "ssh -o StrictHostKeyChecking=no ${user}@${target} uptime"
                         sh "scp -o StrictHostKeyChecking=no -i '/root/.ssh/skysail.pem' -r aws/ec2-34-246-151-3/apache/conf.d ${user}@${target}:/home/ec2-user/jenkinstarget/apache/"
-                        sh "sudo service httpd restart"
+                        sh "ssh -o StrictHostKeyChecking=no ${user}@${target} service httpd restart"
                     }
                 }
             }
