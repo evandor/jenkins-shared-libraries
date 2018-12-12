ed3912af3ead7e636b00891451f42b53c901ca54
Carsten Graef <evandor@gmail.com>
2018-12-12 07:55:49 +0100

    pipeline stuff

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -20,7 +20,7 @@ def call(user, target) {
             stage ('Copy dockerRun to target') {
                 steps{
                     sshagent(credentials : ['sailor1']) {
-                        sh "ssh -o StrictHostKeyChecking=no ${user}@${target} uptime ${project}"
+                        sh "ssh -o StrictHostKeyChecking=no ${user}@${target} uptime"
                         sh "ssh -o StrictHostKeyChecking=no ${user}@${target} whoami"
                         sh "scp -o StrictHostKeyChecking=no docker/dockerRun ${user}@${target}:/home/${user}/bin/dockerRun"
                     }
