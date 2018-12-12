b6d03fe8abd1ca3ee66f549e8fa5c3c3f997c8a9
Carsten Graef <evandor@gmail.com>
2018-12-12 09:02:09 +0100

    pipeline stuff

--- vars/stageServiceDeploy.groovy
+++ vars/stageServiceDeploy.groovy
@@ -1,3 +1,4 @@
 def call(app, env, version) {
-    sh "nohup /home/carsten/install/docker/services/run_docker.sh " + app + " " + env + " " + version
+    //sh "nohup /home/carsten/install/docker/services/run_docker.sh " + app + " " + env + " " + version
+    sh "nohup /home/carsten/bin/dockerRun " + app + " " + env + " " + version
 }
