45fe7bfcb2dd65f1dafdaf080f7fab7a9bd95c0a
Carsten Graef <evandor@gmail.com>
2018-12-25 09:05:28 +0100

    new pipeline

---
+++ vars/buildJar.groovy
@@ -0,0 +1,70 @@
+/**
+ * Pipeline Definition File
+ * Information about the syntax can be found here: https://jenkins.io/doc/book/pipeline/syntax/
+ *
+ * To run this File as a Pipeline job atleast the following plugins are required:
+ * https://wiki.jenkins-ci.org/display/JENKINS/Pipeline+Model+Definition+Plugin
+ * https://wiki.jenkins-ci.org/display/JENKINS/Pipeline+Utility+Steps+Plugin
+ * https://wiki.jenkins-ci.org/display/JENKINS/Lockable+Resources+Plugin
+ */
+
+/*def call(project, modulePath) {
+    call(project, modulePath, true)
+}*/
+
+
+def call(project) {
+
+    env.PROJECT_NAME = project
+
+    pipeline {
+        agent any
+
+        options {
+            buildDiscarder(logRotator(numToKeepStr: '20'))
+        }
+
+        stages {
+
+            stage('Prepare') {
+                steps {
+                    stagePrepareServices()
+                    checkout scm
+                }
+
+            }
+
+            stage('Build') {
+                steps {
+                    sh "cd ${env.MODULE_PATH} && ./gradlew -DbuildVersion=${env.BUILD_VERSION} --stacktrace --continue clean build"
+                }
+                post {
+                    always {
+                        junit "**/test-results/test/TEST-*.xml"
+                    }
+                }
+            }
+
+            stage('Document') {
+                steps {
+                    //sh "./gradlew asciidoctor"
+                    sh "cd ${env.MODULE_PATH} && ./gradlew scaladoc"
+                }
+            }
+
+        }
+        post {
+            failure {
+                emailext body: '$DEFAULT_CONTENT',
+                        recipientProviders: [
+                                [$class: 'CulpritsRecipientProvider'],
+                                [$class: 'DevelopersRecipientProvider'],
+                                [$class: 'RequesterRecipientProvider']
+                        ],
+                        replyTo: '$DEFAULT_REPLYTO',
+                        subject: '$DEFAULT_SUBJECT',
+                        to: '$DEFAULT_RECIPIENTS'
+            }
+        }
+    }
+}
