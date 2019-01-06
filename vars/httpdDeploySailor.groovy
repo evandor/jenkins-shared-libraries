06d10654e1dffd8c0e05ccdfb1519a921e01a617
Carsten Graef <evandor@gmail.com>
2019-01-06 08:15:27 +0100

    moving bootadmin to evandor.de

---
+++ vars/httpdDeploySailor.groovy
@@ -0,0 +1,33 @@
+def call(user, target) {
+
+    pipeline {
+        agent any
+
+        options {
+            buildDiscarder(logRotator(numToKeepStr: '20'))
+        }
+
+        stages {
+
+            stage('Prepare') {
+                steps {
+                    //stagePrepareServices()
+                    checkout scm
+                }
+
+            }
+
+            stage ('Copy dockerRun to target...') {
+                steps{
+                    sshagent(credentials : ['sailor1']) {
+                        sh "ssh -o StrictHostKeyChecking=no ${user}@${target} uptime"
+                        //sh "scp -o StrictHostKeyChecking=no -i '/root/.ssh/skysail.pem' -r aws/ec2-34-246-151-3/apache/conf.d ${user}@${target}:/home/ec2-user/jenkinstarget/apache/"
+                        //sh "scp -o StrictHostKeyChecking=no -r sites-available/ ${user}@${target}:/etc/apache2/sites-available"
+                        sh "ssh -o StrictHostKeyChecking=no ${user}@${target} sudo service httpd restart"
+                    }
+                }
+            }
+
+        }
+    }
+}
