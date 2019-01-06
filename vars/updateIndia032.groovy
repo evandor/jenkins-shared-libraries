28df1aa58dd53b47ddde9883e752ef411fa7336d
Carsten Graef <evandor@gmail.com>
2019-01-06 09:05:18 +0100

    moving sofia-sites-available

--- vars/updateIndia032.groovy
+++ vars/updateIndia032.groovy
@@ -17,10 +17,17 @@ def call() {
 
             }
 
-            stage('Copy files...') {
+            stage('Copy crontab files...') {
                 steps {
                     sh "cp india032/crontab/cert-renew.sh /home/carsten/bin/cert-renew.sh"
                     sh "chmod 775 /home/carsten/bin/cert-renew.sh"
+                }
+            }
+
+            stage ('Copy apache files...') {
+                steps{
+                    sh "cp -r india032/sites-available/ /etc/apache2"
+                    sh "sudo services apache2 reload"
                 }
             }
 
